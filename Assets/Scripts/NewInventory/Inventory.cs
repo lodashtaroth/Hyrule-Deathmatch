052595e872f60ee3fdf4bb0f4be0fe34070f25a9
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2019-01-10 19:53:42 +0800

    steve

---
+++ Assets/Scripts/NewInventory/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public Item item1;
+    public Item item2;
+
+    public Item[] itemsToPickup;
+
+    public void Awake()
+    {
+        item1 = null;
+        item2 = null;
+    }
+
+    void Start()
+    {
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Item" && Input.GetButton("Fire1_P1"))
+        {
+
+            item1 = other.GetComponent<Item>();
+
+
+        }
+        if (other.tag == "Item" && Input.GetButton("Fire2_P1"))
+        {
+
+            item2 = other.GetComponent<Item>();
+
+
+        }
+    }
+}
